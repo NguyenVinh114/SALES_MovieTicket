56234515786afee49fe8f5842295d6d8d36b42f5
NguyenVinh114 <nguyenvinh882821@gmail.com>
2024-04-27 04:19:23 +0700

    fix databse

--- Movie ticket sales management app/DAL/Connection.cs
+++ Movie ticket sales management app/DAL/Connection.cs
@@ -9,7 +9,7 @@ namespace Movie_ticket_sales_management_app.DAL
 {
     internal class Connection
     {
-        private static string stringConnection = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\PC\Desktop\Quản lí bán vé xem phim\Movie ticket sales management app\Movie ticket sales management app\Database1.mdf;Integrated Security=True";
+        private static string stringConnection = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=""C:\Users\PC\Desktop\Movie ticket sales management app\Movie ticket sales management app\DatabaseTaiKhoan.mdf"";Integrated Security=True";
         public static SqlConnection GetSqlConnection()
         {
             return new SqlConnection(stringConnection);

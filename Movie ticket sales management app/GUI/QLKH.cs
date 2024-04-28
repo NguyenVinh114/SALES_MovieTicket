9e56524cce85d3e317672165689801edef89cad8
HieuNotGood <pdhhieu27032004@gmail.com>
2024-04-28 11:03:29 +0700

    Up QLKH

---
+++ Movie ticket sales management app/GUI/QLKH.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Movie_ticket_sales_management_app.GUI
+{
+    public partial class QLKH : Form
+    {
+        public QLKH()
+        {
+            InitializeComponent();
+        }
+    }
+}
